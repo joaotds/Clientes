656120d7a7023a6e20823fef05fb1764c07e9541
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-11-20 09:45:01 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,52 +1,35 @@
 package modelo;
 
 public abstract class Cliente {
-
 	
 	protected Integer id;
 	protected String nomeSocial;
 	protected String email;
 	
-	//metodos
+	//Métodos
 	public abstract String getNome();
 	public abstract String getNroDoc();
 	public abstract String getTipo();
-	/**
-	 * @return the id
-	 */
+	public abstract String getDados();
+	
+	//GETs e SETs
 	public Integer getId() {
 		return id;
 	}
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	/**
-	 * @return the nomeSocial
-	 */
 	public String getNomeSocial() {
 		return nomeSocial;
 	}
-	/**
-	 * @param nomeSocial the nomeSocial to set
-	 */
 	public void setNomeSocial(String nomeSocial) {
 		this.nomeSocial = nomeSocial;
 	}
-	/**
-	 * @return the email
-	 */
 	public String getEmail() {
 		return email;
 	}
-	/**
-	 * @param email the email to set
-	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
 	
 }
